5d2d7cb48fb3ae1362edc4fb28591e2605648608
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-04-08 15:45:21 +0200

    feat: add users command

--- server/src/commands/users.c
+++ server/src/commands/users.c
@@ -6,10 +6,56 @@
 */
 
 #include "types.h"
+#include "commands.h"
+#include "protocol.h"
+
+#include <string.h>
+#include <stdlib.h>
+
+static int getSizeMessage(user_list_t *user_list)
+{
+    int size = 0;
+    user_t *tmp = user_list->first;
+
+    while (tmp != NULL) {
+        size += strlen(tmp->username) + 3 + strlen(tmp->uuid) + 2;
+        tmp = tmp->next;
+    }
+    return size;
+}
+
+static char *getMessage(user_list_t *user_list)
+{
+    int size = getSizeMessage(user_list);
+    char *msg = malloc(sizeof(char) * (size + 1));
+    user_t *tmp = user_list->first;
+
+    memset(msg, 0, size + 1);
+    strcat(msg, "[");
+    while (tmp->next != NULL) {
+        strcat(msg, tmp->username);
+        strcat(msg, " : ");
+        strcat(msg, tmp->uuid);
+        strcat(msg, ", ");
+        tmp = tmp->next;
+    }
+    strcat(msg, tmp->username);
+    strcat(msg, " : ");
+    strcat(msg, tmp->uuid);
+    strcat(msg, "]");
+    return msg;
+}
 
 void users_command(char **command, server_t *myServ, client_t *client)
 {
-    (void) command;
-    (void) myServ;
-    (void) client;
+    user_list_t *user_list = myServ->_list_users;
+    char *msg = NULL;
+
+    if (is_correct_command(&myServ->writefds, command, 0, client->_fd) == false)
+        return;
+    msg = getMessage(user_list);
+    ptc_send(LOGED_IN, msg, client->_fd, &myServ->writefds);
+    free(msg);
 }
+
+// CODE [user1 : uuid, user2 : uuid, user3 : uuid, ...]
