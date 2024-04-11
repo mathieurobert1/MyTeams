6d2646c6c79951f52e72ceffaaecffbae454e93c
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-04-11 17:28:52 +0200

    feat (wip): management of the third case of the list command (channel list) added

--- server/src/commands/list.c
+++ server/src/commands/list.c
@@ -8,6 +8,7 @@
 #include "types.h"
 #include "utils.h"  // *
 #include "commands.h"   // use_command_e
+#include "protocol.h"   // ptc_send
 
 static channel_list_t *get_channel_list(char *team_uuid, server_t *myServ)
 {
@@ -18,17 +19,35 @@ static channel_list_t *get_channel_list(char *team_uuid, server_t *myServ)
     return team->channels;
 }
 
+static thread_list_t *get_thread_list(server_t *myServ, char *team_uuid, char *channel_uuid)
+{
+    team_t *team = team_get_by_uuid(team_uuid, myServ->_list_teams);
+    channel_t *channel = NULL;
+
+    if (!team)
+        return NULL;
+    channel = channel_get_by_uuid(channel_uuid, team->channels);
+    if (!channel)
+        return NULL;
+    return channel->threads;
+}
+
 void list_command(char **command, server_t *myServ, client_t *client)
 {
     char *str = NULL;
     channel_list_t *channels = NULL;
+    thread_list_t *threads = NULL;
 
-    if (!is_command_correct(command, 0, client))
+    if (!is_correct_command(&myServ->writefds, command, 0, client->_fd))
         return;
     if (client->_use_state == EMPTY) {
         str = get_str_list_all_teams(myServ->_list_teams);
     } if (client->_use_state == TEAM) {
         channels = get_channel_list(client->_use_uuid_team, myServ);
         str = get_str_list_all_channels(channels);
+    } if (client->_use_state == CHANNEL) {
+        threads = get_thread_list(myServ, client->_use_uuid_team, client->_use_uuid_channel);
+        str = get_str_list_all_threads(threads);
     }
+    ptc_send(COMMAND_SUCCESS, str, client->_fd, &myServ->writefds);
 }
