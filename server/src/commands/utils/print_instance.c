e0528e903824df48ec27bf75780f6fdc4d539124
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-04-15 16:10:47 +0200

    refractor: list_instance to print_instance,   update all the print functions in the message, which no longer return anything

---
+++ server/src/commands/utils/print_instance.c
@@ -0,0 +1,69 @@
+/*
+** EPITECH PROJECT, 2024
+** MyTeams
+** File description:
+** list_instance
+*/
+
+#include "types.h" // *
+#include "protocol.h" // *
+
+#include <stddef.h> // null
+#include <string.h> // strcmp
+#include <stdlib.h> // malloc
+#include <stdio.h> // dprintf
+
+void print_list_all_teams(team_list_t *teams, int fd, fd_set *writefds)
+{
+    team_t *tmp = teams->first;
+
+    while (tmp) {
+        if (FD_ISSET(fd, writefds) == 0)
+            return;
+        dprintf(fd, "%d \"%s\" \"%s\"\r\n", CLIENT_PRINT_TEAM, tmp->uuid,
+            tmp->name);
+        tmp = tmp->next;
+    }
+}
+
+void print_list_all_threads(thread_list_t *threads, int fd, fd_set *writefds)
+{
+    thread_t *tmp = threads->first;
+
+    while (tmp) {
+        if (FD_ISSET(fd, writefds) == 0)
+            return;
+        dprintf(fd, "%d \"%s\" \"%s\" \"%s\"\r\n", CLIENT_PRINT_THREAD,
+            tmp->uuid, tmp->title, tmp->content);
+        tmp = tmp->next;
+    }
+}
+
+void print_list_all_channels(channel_list_t *channels, int fd,
+    fd_set *writefds)
+{
+    channel_t *tmp = channels->first;
+
+    while (tmp) {
+        if (FD_ISSET(fd, writefds) == 0)
+            return;
+        dprintf(fd, "%d \"%s\" \"%s\"\r\n", CLIENT_PRINT_CHANNEL, tmp->uuid,
+            tmp->name);
+        tmp = tmp->next;
+    }
+}
+
+void print_list_all_comments(thread_t *thread, int fd, fd_set *writefds)
+{
+    comment_list_t *comments = thread->comments;
+    comment_t *tmp = comments->first;
+
+    while (tmp) {
+        if (FD_ISSET(fd, writefds) == 0)
+            return;
+        dprintf(fd, "%d \"%s\" \"%s\" \"%ld\", \"%s\"\r\n",
+            CLIENT_THREAD_PRINT_REPLIES, thread->uuid, tmp->uuid,
+            tmp->timestamp, tmp->content);
+        tmp = tmp->next;
+    }
+}
