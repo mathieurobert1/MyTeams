f7fdba7ea2b2da7c705f79d7ca24759bf8e5cda9
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-04-19 17:42:51 +0200

    feat: check if we can write on file descriptor for load and save server

--- server/src/data/load.c
+++ server/src/data/load.c
@@ -16,11 +16,20 @@ server_t *data_load(void)
 {
     int fd = open("server.db", O_RDONLY | O_CREAT, 0666);
     server_t *server = NULL;
+    fd_set readfds;
 
     if (fd == -1) {
         write(1, "Error with open in data_save\n", 30);
         return NULL;
     }
+    FD_ZERO(&readfds);
+    FD_SET(fd, &readfds);
+    if (select(fd + 1, &readfds, NULL, NULL, NULL) == -1) {
+        write(1, "Error with select in data_save\n", 32);
+        return NULL;
+    }
+    if (!FD_ISSET(fd, &readfds))
+        return NULL;
     server = deserialize_server(fd);
     if (server == NULL) {
         write(1, "Error with malloc in data_load\n", 32);
--- server/src/data/save.c
+++ server/src/data/save.c
@@ -14,9 +14,19 @@
 void data_save(server_t *server)
 {
     int fd = open("server.db", O_CREAT | O_WRONLY | O_TRUNC, 0666);
+    fd_set writefds;
+    int result_select = 0;
 
     if (fd == -1) {
         write(1, "Error with open in data_save\n", 30);
     }
-    serialize_server(fd, server);
+    FD_ZERO(&writefds);
+    FD_SET(fd, &writefds);
+    result_select = select(fd + 1, NULL, &writefds, NULL, NULL);
+    if (result_select == -1) {
+        write(1, "Error with select in data_save\n", 32);
+    }
+    if (FD_ISSET(fd, &writefds)) {
+        serialize_server(fd, server);
+    }
 }
