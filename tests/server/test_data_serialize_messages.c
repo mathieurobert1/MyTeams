4e1acea75d8a43a4977a7de2f6fa233c265bec0c
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-04-21 19:53:48 +0200

    feat: add test for serialize message list fct

--- tests/server/test_data_serialize_messages.c
+++ tests/server/test_data_serialize_messages.c
@@ -37,3 +37,49 @@ Test(serialize_message, serialization_test) {
 
     revoke(filename);
 }
+
+Test(serialize_message_list, serialization_test) {
+    char filename[] = "temp_test_file_message_list";
+    int fd = creat(filename, 0644);
+
+    message_t message1 = {.sender_uuid = "sender1", .receiver_uuid = "receiver1", .message = "Message1"};
+    message_t message2 = {.sender_uuid = "sender2", .receiver_uuid = "receiver2", .message = "Message2"};
+    message_t message3 = {.sender_uuid = "sender3", .receiver_uuid = "receiver3", .message = "Message3"};
+
+    (void)fd;
+    (void)filename;
+    message_list_t *list = (message_list_t *)malloc(sizeof(message_list_t));
+    list->first = &message1;
+    message1.next = &message2;
+    message2.next = &message3;
+    message3.next = NULL;
+
+    serialize_message_list(fd, list);
+
+    close(fd);
+
+    fd = open(filename, O_RDONLY);
+
+    message_list_t read_list;
+    read(fd, &read_list, sizeof(message_list_t));
+    cr_assert_not_null(read_list.first, "Serialized message list is NULL");
+
+    message_t *tmp = read_list.first;
+    cr_assert_str_eq(tmp->sender_uuid, "sender1", "Serialized sender UUID of message1 incorrect");
+    cr_assert_str_eq(tmp->receiver_uuid, "receiver1", "Serialized receiver UUID of message1 incorrect");
+    cr_assert_str_eq(tmp->message, "Message1", "Serialized message1 incorrect");
+
+    tmp = tmp->next;
+    cr_assert_str_eq(tmp->sender_uuid, "sender2", "Serialized sender UUID of message2 incorrect");
+    cr_assert_str_eq(tmp->receiver_uuid, "receiver2", "Serialized receiver UUID of message2 incorrect");
+    cr_assert_str_eq(tmp->message, "Message2", "Serialized message2 incorrect");
+
+    tmp = tmp->next;
+    cr_assert_str_eq(tmp->sender_uuid, "sender3", "Serialized sender UUID of message3 incorrect");
+    cr_assert_str_eq(tmp->receiver_uuid, "receiver3", "Serialized receiver UUID of message3 incorrect");
+    cr_assert_str_eq(tmp->message, "Message3", "Serialized message3 incorrect");
+
+    close(fd);
+
+    revoke(filename);
+}
